b393f1495244d07594ef8d4bcbf962d78c4e43b8
Gugulugu <79146263+Gugulugu@users.noreply.github.com>
2021-03-11 18:13:51 +0100

    rem_row_na still buggy

--- DataScienceTool/Gui.py
+++ DataScienceTool/Gui.py
@@ -9,9 +9,13 @@ def load_file():
     try:
         dataset = pd.read_csv(textfield.get())
         messagebox.showinfo("Message", "File has been loaded successfully in")
-
     except:
         messagebox.showinfo("Message", "please enter a valid path")
+
+
+def Rem_row_na():
+    dataset = pd.read_csv(textfield.get())
+    dataset.dropna()
 
 
 # execute when button pressed
@@ -26,12 +30,18 @@ window.title("Data Science Tool")
 # window size
 window.geometry("500x500")
 
+# define RadioButtons
+var = IntVar()
+R1 = Radiobutton(window, text="Remove Rows with NA", variable=var, value=1,
+                 command=Rem_row_na)
+R1.grid(row=1, column=1)
+
 # create Textfield
 textfield = Entry(window, bd=5, width=40)
 
 # Label=Text for buttons
 # create Labels
-ReplaceNa_label = Label(window, text="Replace all NA with 0")
+RemoveNa_label = Label(window, text="Replace all NA with 0")
 exit_label = Label(window, text="Close Window")
 textfield_label = Label(window, text="Enter Path of File")
 
@@ -39,21 +49,18 @@ textfield_label = Label(window, text="Enter Path of File")
 textfield = Entry(window, bd=5, width=40)
 
 # create Buttons
-ReplaceNA_button = Button(window, text="Replace", command=button_action)
+RemoveNA_button = Button(window, text="Remove", command=button_action)
 exit_button = Button(window, text="Exit", command=window.quit)
 Load_in_file = Button(window, text="Load File", command=load_file)
 
 # positioning of the items
-ReplaceNa_label.grid(row=1, column=1, columnspan=4)
-ReplaceNA_button.grid(row=1, column=10)
+RemoveNa_label.grid(row=4, column=1, columnspan=4)
+RemoveNA_button.grid(row=4, column=10)
 exit_label.grid(row=5, column=1)
 exit_button.grid(row=5, column=10)
 textfield_label.grid(row=9, column=1)
 textfield.grid(row=9, column=10)
 Load_in_file.grid(row=9, column=15)
 
-
 # call window and wait for user to enter
 window.mainloop()
-
-
